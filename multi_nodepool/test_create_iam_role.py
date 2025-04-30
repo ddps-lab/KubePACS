9a0ae520018f07f9551c480658d118b7542398c4
TaeYoon <tykim7@kookmin.ac.kr>
2025-04-30 13:21:46 +0900

    del: unused test code

--- multi_nodepool/test_create_iam_role.py
+++
@@ -1,6 +0,0 @@
-from kubecaps_scaler import create_node_role
-
-print(create_node_role(
-    region="us-east-1",
-    cluster_name="mhsong-k8s-cluster"
-))
--- multi_nodepool/test_node_deletion.py
+++
@@ -1,6 +0,0 @@
-from kubecaps_scaler import delete_spot_instance_for_eks
-
-delete_spot_instance_for_eks(
-    instance_id="i-050424bda491aaf86",
-    region="us-east-1"
-)
